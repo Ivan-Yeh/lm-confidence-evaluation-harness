c5c9481dfc8915ee3ef1610f69541c472f99f6dc
Ivan-Yeh <ivan.hsuu@gmail.com>
2025-12-23 09:19:34 +1100

    updated trivia qa setup and scripts

--- scripts/trivia_qa_dist.sh
+++ scripts/trivia_qa_dist.sh
@@ -1,27 +1,28 @@
 #!/usr/bin/env bash
 
-GPU=0
-limit=200
+GPU=1
+limit=null
+rounds=5
 
 models=(
     "meta-llama/Meta-Llama-3-8B-Instruct"
     "meta-llama/Llama-3.1-8B-Instruct"
     "openai/gpt-oss-20b"
     "qwen/Qwen2.5-7B-Instruct"
-    "Qwen/Qwen3-8B"
+    "qwen/Qwen3-8B"
+    "mistralai/Mistral-7B-Instruct-v0.3"
+    "google/gemma-3-12b-it"
 )
 
 tasks=(
-    "lnll"
-    "linguistic_confidence"
-    "semantic_uncertainty"
     "dist_lnll"
     "dist_linguistic_confidence"
+    "dist_p_true_mc"
     "dist_semantic_uncertainty"
 )
 
 for model in "${models[@]}"; do
     for task in "${tasks[@]}"; do
-        CUDA_VISIBLE_DEVICES=$GPU python -m lm_conf limit=$limit dataset=trivia_qa task=$task qa_model.name=$model
+        CUDA_VISIBLE_DEVICES=$GPU python -m lm_conf dataset=trivia_qa rounds=$rounds limit=$limit task=$task qa_model.name=$model qa_model.reasoning_effort=low qa_model.max_tokens=512 
     done
 done
--- scripts/trivia_qa_scalar.sh
+++ scripts/trivia_qa_scalar.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 GPU=0
-limit=200
-
+limit=null
+rounds=5
 
 models=(
     "meta-llama/Meta-Llama-3-8B-Instruct"
@@ -10,19 +10,19 @@ models=(
     "openai/gpt-oss-20b"
     "qwen/Qwen2.5-7B-Instruct"
     "qwen/Qwen3-8B"
+    "mistralai/Mistral-7B-Instruct-v0.3"
+    "google/gemma-3-12b-it"
 )
 
 tasks=(
     "lnll"
     "linguistic_confidence"
+    "p_true_mc"
     "semantic_uncertainty"
-    "dist_lnll"
-    "dist_linguistic_confidence"
-    "dist_semantic_uncertainty"
 )
 
 for model in "${models[@]}"; do
     for task in "${tasks[@]}"; do
-        CUDA_VISIBLE_DEVICES=$GPU python -m lm_conf limit=$limit dataset=trivia_qa task=$task qa_model.name=$model qa_model.reasoning_effort=low qa_model.max_tokens=512 
+        CUDA_VISIBLE_DEVICES=$GPU python -m lm_conf dataset=trivia_qa rounds=$rounds limit=$limit task=$task qa_model.name=$model qa_model.reasoning_effort=low qa_model.max_tokens=512 
     done
 done
