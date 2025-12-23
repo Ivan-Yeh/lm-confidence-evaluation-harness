59216a3cd3d5a92117691e8e790362b00a4135a5
Ivan Yeh <ivan.hsuu@gmail.com>
2025-12-24 03:38:07 +1100

    update dAUROC

--- lm_conf/post_processing/metrics.py
+++ lm_conf/post_processing/metrics.py
@@ -309,7 +309,7 @@ def dAUROC(cfg: dict, extracted_output: OrganisedOutputs) -> list[float]:
     if not pos_dists or not neg_dists:
         return [float("nan")]
 
-    num_dauroc_mc = cfg.get("num_dauroc_mc", 1_000_000)
+    num_dauroc_mc = cfg.get("num_dauroc_mc", 10_000_000)
     seed = cfg.get("seed", None)
 
     if seed is not None:
