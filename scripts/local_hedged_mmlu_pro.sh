bc012f64b93f0ddebc7c52ddacbd2306eef2513b
Ivan Yeh <ivan.hsuu@gmail.com>
2026-03-29 22:26:12 +1100

    added local inference scripts

---
+++ scripts/local_hedged_mmlu_pro.sh
@@ -0,0 +1,51 @@
+# #!/usr/bin/env bash
+
+GPU=0
+limit=null
+rounds=1
+max_model_len=2048
+max_tokens=256
+dataset=mmlu_pro
+backend=vllm
+
+
+CUDA_VISIBLE_DEVICES=$GPU python -m lm_conf dataset=$dataset \
+    rounds=$rounds \
+    limit=$limit \
+    task=hedged_qa_unified_tp \
+    qa_model.name=openai/gpt-oss-20b \
+    qa_model.backend=$backend \
+    qa_model.reasoning_effort=low \
+    qa_model.max_tokens=$max_tokens \
+    qa_model.max_model_len=$max_model_len 
+
+CUDA_VISIBLE_DEVICES=$GPU python -m lm_conf dataset=$dataset \
+    rounds=$rounds \
+    limit=$limit \
+    task=hedged_qa_unified_tp \
+    qa_model.name=meta-llama/Llama-3.1-8B-Instruct \
+    qa_model.backend=$backend \
+    qa_model.reasoning_effort=low \
+    qa_model.max_tokens=$max_tokens \
+    qa_model.max_model_len=$max_model_len 
+
+CUDA_VISIBLE_DEVICES=$GPU python -m lm_conf dataset=$dataset \
+    rounds=$rounds \
+    limit=$limit \
+    task=hedged_qa_unified_tp \
+    qa_model.name=qwen/Qwen3-8B \
+    qa_model.backend=$backend \
+    qa_model.reasoning_effort=null \
+    qa_model.max_tokens=$max_tokens \
+    qa_model.max_model_len=$max_model_len 
+
+CUDA_VISIBLE_DEVICES=$GPU python -m lm_conf dataset=$dataset \
+    rounds=$rounds \
+    limit=$limit \
+    task=hedged_qa_unified_tp \
+    qa_model.name=mistralai/Mistral-7B-Instruct-v0.3 \
+    qa_model.backend=$backend \
+    qa_model.reasoning_effort=low \
+    qa_model.max_tokens=$max_tokens \
+    qa_model.max_model_len=$max_model_len 
+
